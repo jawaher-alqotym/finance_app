971323d552a76eade21952c48687813edd61dc91
Hawra Alomani <hawraalomani@Hawras-MacBook-Pro.local>
2022-11-01 22:13:25 +0300

    added my changes including splash and onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,14 @@ import 'package:finance_app/screens/add_new_expense_page.dart';
 import 'package:finance_app/screens/insight_page.dart';
 import 'package:finance_app/controllers_binding.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:finance_app/screens/onboarding_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  // for splash screen
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -18,11 +24,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  // For the splash screen, delayed 3 sec
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(seconds: 3)).then((value)=>{
+      FlutterNativeSplash.remove()
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: OnBoardingPage(),
       initialBinding: ControllerBindings(),
     );
   }
